dc298033dfdcc06844a6ca2b205ee40dd59be54c
acp129 <acpanchumarthi@gmail.com>
2021-05-20 21:48:01 -0400

    added description of problem

--- src/OrdinaryNumbers.java
+++ src/OrdinaryNumbers.java
@@ -1,15 +1,24 @@
+/*
+A +ve number is ordinary if all digits in its base 10 representation are the same.
+eg. 1, 2, 3, 77, 99, and 2222 are ordinary numbers
+Program will take in a value n t times, where for each n, we need to output # of ordinary numbers from 1 to n.
+ */
+
+/*
+Everytime floor(log10(n)) increases by 1, the output is to be increased by 9.
+Just make sure for the final iteration (# of digits equal to # of digits in n), you check for each ordinary number.
+ */
+
 
 import java.util.Scanner;
 
 public class OrdinaryNumbers {
 
     public static void main(String[] args) {
-        // write your code here
-        int n,t;
         Scanner scan = new Scanner(System.in);
-        t = scan.nextInt();
-        for(int i = 0;i<t ;i++){
-            n = scan.nextInt();
+        int t = scan.nextInt();
+        for(int i = 0;i < t ;i++){
+            int n = scan.nextInt();
             int temp = n;
             int size = 0;
             int num = 0;
